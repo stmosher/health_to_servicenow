2e6cc3291ded762f7f29c07124f0f278bf920cea
stmosher <Datacomm2>
2019-09-09 12:54:46 -0400

    added alert parsing and service now POST call

--- test_webserver.py
+++ test_webserver.py
@@ -24,7 +24,90 @@ if __name__ == '__main__':
 
     web_server = 'http://127.0.0.1:5001'
     view_route = '/health_to_snow'
-    data = {'one': '1', 'two': '2', 'three': '3'}
-    url = web_server + view_route
 
+    data = [{
+        "series": [{
+            "columns": [
+                "time",
+                "activation_threshold",
+                "alert_src",
+                "clear_threshold",
+                "crit_threshold",
+                "id",
+                "kpi_stream",
+                "msg",
+                "sigma",
+                "warn_threshold"
+            ],
+            "name": "alerts",
+            "tags": {
+                "CollectorId": "mdt-robot-collector-13a3958e-63d5-47e5-9e0f-31e846b89f2e",
+                "EncodingPath": "Cisco-IOS-XR-wdsysmon-fd-oper:system-monitoring/cpu-utilization",
+                "Producer": "spnac-a9k-s079",
+                "UUID": "5108e98e-aea0-49a5-ba32-88db39b777c3",
+                "kpi_id": "pulse_cpu_utilization",
+                "level": "INFO",
+                "node-name": "0/RP0/CPU0",
+                "state": "clear"
+            },
+            "values": [
+                [
+                    "2019-09-05T22:02:57.412Z",
+                    -1,
+                    "TICK",
+                    2,
+                    2,
+                    "pulse_cpu_utilization",
+                    2,
+                    "INFO : 0/RP0/CPU0  5min CPU Utilization: 2.00 % has returned to Usual range, Threshold: 2.00 SD. It is 0.45 std. dev away from Average.",
+                    0.45390323755854023,
+                    2
+                ]
+            ]
+        }]
+    },
+        {
+            "series": [{
+                "columns": [
+                    "time",
+                    "activation_threshold",
+                    "alert_src",
+                    "clear_threshold",
+                    "crit_threshold",
+                    "id",
+                    "kpi_stream",
+                    "msg",
+                    "sigma",
+                    "warn_threshold"
+                ],
+                "name": "alerts",
+                "tags": {
+                    "CollectorId": "mdt-robot-collector-13a3958e-63d5-47e5-9e0f-31e846b89f2e",
+                    "EncodingPath": "Cisco-IOS-XR-infra-statsd-oper:infra-statistics/interfaces/interface/latest/data-rate",
+                    "Producer": "spnac-a9k-s078",
+                    "UUID": "16df73dc-0614-44bd-885a-1cad09a68689",
+                    "interface-name": "GigabitEthernet0/0/0/0",
+                    "kpi_id": "pulse_interface_rate_counters",
+                    "level": "INFO",
+                    "state": "clear"
+                },
+                "values": [
+                    [
+                        "2019-09-05T22:03:03.496Z",
+                        -1,
+                        "TICK",
+                        2,
+                        2,
+                        "pulse_interface_rate_counters_rxrate",
+                        0,
+                        "INFO : GigabitEthernet0/0/0/0 Rx Rate: 0.00 pkts/sec has returned to Usual range, Threshold: 2.00 SD. It is 0.00 std.dev away from Average.",
+                        0,
+                        2
+                    ]
+                ]
+            }]
+        }
+    ]
+
+    url = web_server + view_route
     test_server(url, data)
--- web_server.py
+++ web_server.py
@@ -1,18 +1,75 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from threading import Thread
+import requests
+import json
 
 
 app = Flask(__name__)
 
 
+def post_to_snow(alert_dict):
+    u = 'https://dev82732.service-now.com/api/now/table/x_397387_cw_alerts_alert_table'
+    username = 'admin'
+    password = 'XXX'
+
+    headers = {'Accept': 'application/json',
+               'Content-type': 'application/json'}
+
+    r = requests.post(u, auth=(username, password), headers=headers, data=json.dumps(alert_dict))
+
+    if r.status_code != 200:
+        print('Web server communication error {}'.format(r.status_code))
+
+
+def my_zip(list1, list2):
+    md = dict()
+    for i in list1:
+        md[i] = list2[list1.index(i)]
+    return md
+
+
+def parse_alert_body(body):
+    """
+        This parses the body of the alert message, processes into simple dictionaries, and return a list
+        of dictionaries composed of the seven key, value pairs as seen in the add_dicts dictionary
+        below.
+    """
+    results = list()
+    for a in body:
+        for s in a['series']:
+            columns_list = [c for c in s['columns']]
+            for v1 in s['values']:
+                values_list = [v for v in v1]
+                z_dict = my_zip(columns_list, values_list)
+                add_dict = {'producer': s['tags']['Producer'], 'kpi_id': s['tags']['kpi_id'],
+                            'level': s['tags']['level'], 'state': s['tags']['state'], 'uuid': s['tags']['UUID'],
+                            'time': z_dict['time'], 'id': z_dict['id']}
+                results.append(add_dict)
+    return results
+
+
+def thread_waiter(alert):
+    """
+        This relieves Flask from waiting for action.
+    """
+    results = parse_alert_body(alert)  # results is list of dicts
+    for i in results:
+        post_to_snow(i)
+
+
 @app.route('/health_to_snow', methods=['GET', 'POST'])
 def health_to_snow():
+    """
+        Receive alert from crossworks, return status 200, and kick off thread for further processing
+    """
     if request.method == 'GET':
         reply = 'Use the POST method'
         return reply
     elif request.method == 'POST':
         data = request.json
+        Thread(target=thread_waiter, args=(data,)).start()
         return jsonify(data)
 
 
